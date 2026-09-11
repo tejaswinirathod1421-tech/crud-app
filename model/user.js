6d1c1a57a7149c4d5dc87868a71a0278226b0704
Rathod Tejaswini <tejaswinirathod1421@gmail.com>
2026-09-11 07:27:38 +0000

    user file is updated

--- model/user.js
+++ model/user.js
@@ -1,1 +1,12 @@
-console.log("user file is opened");
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+    name: String,
+    email: String,
+    age: Number
+});
+MediaSourceHandle.exports = mongoose.model('User', userSchema);
